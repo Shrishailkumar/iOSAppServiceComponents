8770b1e3ecd59c52e07622e109312ba7514cfba7
shekharVishwakarma <shekhar.vishwakarma@zensar.com>
2022-07-19 15:08:02 +0530

    Initial project setup

---
+++ ZDeviceHelper/ZDeviceHelper/ZDeviceHelper.swift
@@ -0,0 +1,14 @@
+//
+//  ZDeviceHelper.swift
+//  ZDeviceHelper
+//
+//  Created by SV59349 on 19/07/22.
+//
+
+import Foundation
+
+public class ZDeviceHelper {
+    public class func logToConsole(msg: String) {
+        print(msg)
+    }
+}
