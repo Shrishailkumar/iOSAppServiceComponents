3410a559c0d4c8ce28e077cc33a566dc5a948e57
Jitendra Kumar <jitendra.kumar@zensar.com>
2022-07-28 14:51:45 +0530

    Geofencing component initials stage

---
+++ GeofencingServices/GeofencingServices/GeofencingServices.h
@@ -0,0 +1,18 @@
+//
+//  GeofencingServices.h
+//  GeofencingServices
+//
+//  Created by Jitendra Kumar on 28/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GeofencingServices.
+FOUNDATION_EXPORT double GeofencingServicesVersionNumber;
+
+//! Project version string for GeofencingServices.
+FOUNDATION_EXPORT const unsigned char GeofencingServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeofencingServices/PublicHeader.h>
+
+
